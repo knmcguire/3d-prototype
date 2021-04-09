bbfc41ebe53e136d7a6b5d4bd09a4dc7b4f2b1c5
knmcguire <k.n.mcguire@tudelft.nl>
2021-04-09 08:39:34 +0200

    first commit

---
+++ Assets/Asset Unlock - 3D Prototyping/Scripts/CountBalls.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CountBalls : MonoBehaviour
+{
+    public TextMeshProUGUI CounterText;
+
+    [SerializeField] private int Count = 0;
+
+    private void Start()
+    {
+        Count = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.gameObject.CompareTag("Ball")&&other.gameObject!=null){
+            //other.gameObject.SetActive(false);
+            SwarmBall swarmBall = other.gameObject.GetComponent<SwarmBall>();
+            swarmBall.IsCollected = true;
+            Count += 1;
+            CounterText.text = "Count : " + Count;
+
+        }
+
+    }
+}
---
+++ Assets/Asset Unlock - 3D Prototyping/Scripts/SpawnManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]  float spawnRangeX = 20;
+    [SerializeField] float spawnRangeZ = 20;
+
+    [SerializeField] float spawnPosY = 1;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void  SpawnRandomBall()
+    {
+        GameObject pooledBall = ObjectPooler.SharedInstance.GetPooledObject();
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
+            spawnPosY, Random.Range(-spawnRangeZ, spawnRangeZ));
+         if (pooledBall != null)
+            {
+                pooledBall.SetActive(true); // activate it
+                pooledBall.transform.position = spawnPos; 
+            }
+    }
+
+}
